c61ffdb80225f1ddac866fd59501bcab2ab35980
alba <alba2006gs@gmail.com>
2025-04-28 12:51:55 +0200

    subiendo el main

---
+++ main.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+import archivo as archivo 
+def main():
+    a = 3
+    b = 6 
+    c = archivo.suma(a, b)
+
+    print(f"el valor es {c}")
+
+if __name__ == "__main__":
+    main()
+=======
+def sumar(a,b):
+    return a +b 
+>>>>>>> origin/branchM
